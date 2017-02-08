b8730934827f28e657e360c7ef2cce7b9d92493a
dimdim <1661359@mail.ru>
2017-02-08 11:27:47 +0300

    Revert "Revert "added UserNameValidation""
    This reverts commit 827080c26f543d52fe418d101d522004760cbb8f.

--- UserApp/UserApp/ViewModel/LoginViewModel.cs
+++ UserApp/UserApp/ViewModel/LoginViewModel.cs
@@ -1,17 +1,21 @@
-﻿    using System.Threading;
-    using System.Threading.Tasks;
-    using System.Windows.Input;
-    using PropertyChanged;
-    using UserApp.Pages;
-    using UserApp.Services;
-    using UserApp.Services.ApiWrapper;
-    using UserApp.Shared.ViewModels;
-    using Xamarin.Forms;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using IronKit.Validation;
+using IronKit.Validation.Utils;
+using PropertyChanged;
+using UserApp.Pages;
+using UserApp.Services;
+using UserApp.Services.ApiWrapper;
+using UserApp.Shared.ViewModels;
+using Xamarin.Forms;
+
 
 namespace UserApp.ViewModel
 {
     [ImplementPropertyChanged]
-    public class LoginViewModel : IViewModel
+    public class LoginViewModel : IViewModel, IValidatable<LoginViewModel>
     {
         private readonly IApiProvider apiProvider;
         private readonly AppSessionConfig appSessionConfig;
@@ -21,16 +25,21 @@ namespace UserApp.ViewModel
         public ICommand DoLoginCommand
         {
             get;
-            private set;
         }
+
+        [Required]
+        [MinLength(4)]
         public string UserName
         {
             get;
             set;
         }
 
+
         public LoginViewModel(IApiProvider apiProvider, AppSessionConfig appSessionConfig)
         {
+            ValidationInfo = new ValidationInfo<LoginViewModel>(this);
+            ValidationInfo.AddValidator(m => m.UserName.Length < 8, nameof(UserName));
             DoLoginCommand = new Command(async () => await DoLogin(),() => canLogin );
             this.apiProvider = apiProvider;
             this.appSessionConfig = appSessionConfig;
@@ -38,6 +47,8 @@ namespace UserApp.ViewModel
 
         private async Task DoLogin()
         {
+            if (!this.IsValid())
+                return;
             SetCanLoginState(false);
             var cancelationToken = new CancellationTokenSource();
             var authViewModel = await apiProvider.MakeRequest(ct => apiProvider.UsersApi.Login(new UserAuthorizationViewModel {UserName = UserName}, ct), cancelationToken.Token);
@@ -59,5 +70,7 @@ namespace UserApp.ViewModel
                 await Application.Current.MainPage.Navigation.PushAsync(new MainPage());
             }
         }
+
+        public ValidationInfo<LoginViewModel> ValidationInfo { get; }
     }
 }
